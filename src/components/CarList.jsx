a5c5bc31bc7b73d5f3ddeeed326a254a16e7461e
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-09-26 11:06:09 +0530

    Animation added to Drivers and cars -- Project complete

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,21 +1,49 @@
 import { cars } from "../utils/cars";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 function CarList() {
+  const carsRef = useRef([]);
+
+  useEffect(() => {
+    carsRef.current.forEach((carRef, index) => {
+      gsap.fromTo(
+        carRef,
+        {
+          scale:0,
+        },
+        {
+          delay: 0.3,
+          scale: 1,
+          duration: 0.6,
+        }
+      );
+    });
+  }, []);
+
   return (
     <>
       <h2 className="bg-yellow-500 text-white font-extrabold uppercase my-5 text-center py-3 text-2xl md:text-4xl">
         Our Cars
       </h2>
       <ul className="my-4 grid md:grid-cols-3 gap-10 place-items-center">
-        {cars.map((car) => (
-          <li key={car.id} className="mx-4 md:mx-0">
+        {cars.map((car, index) => (
+          <li
+            key={car.id}
+            ref={(elem) => (carsRef.current[index] = elem)}
+            className="mx-4 md:mx-0"
+          >
             <div className="max-w-sm border rounded-lg shadow-md bg-gray-200 border-gray-300 hover:shadow-md hover:shadow-black/60 hover:scale-105 duration-300">
               <img className="rounded-t-lg" src={car.url} alt={car.model} />
               <div className="py-5 px-3 md:px-5 flex items-center justify-between">
                 <h5 className="text-xl md:text-2xl font-semibold tracking-tight text-gray-800 ">
                   {car.model}
                 </h5>
-                <p className={`text-sm md:text-base text-center font-bold ${car.available?"text-green-700":"text-red-700"} uppercase`}>
+                <p
+                  className={`text-sm md:text-base text-center font-bold ${
+                    car.available ? "text-green-700" : "text-red-700"
+                  } uppercase`}
+                >
                   {car.available ? "Available" : "Unavailable"}
                 </p>
               </div>
--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -1,14 +1,38 @@
 import { drivers } from "../utils/drivers";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 function Drivers() {
+  const driversRef = useRef([]);
+  useEffect(() => {
+    driversRef.current.forEach((driverRef, index) => {
+      gsap.fromTo(
+        driverRef,
+        {
+          scale: 0,
+          rotate: 720,
+        },
+        {
+          rotate: 0,
+          delay: 0.3,
+          scale: 1,
+          duration: 0.3,
+        }
+      );
+    });
+  }, []);
+
   return (
     <>
       <h2 className="bg-yellow-500 text-white my-5 uppercase font-extrabold text-center py-3 text-2xl md:text-4xl">
         Our Drivers
       </h2>
       <ul className="my-4 grid md:grid-cols-4 gap-y-10  place-items-center w-full h-full">
-        {drivers.map((driver) => (
-          <li key={driver.id}>
+        {drivers.map((driver, index) => (
+          <li
+            key={driver.id}
+            ref={(elem) => (driversRef.current[index] = elem)}
+          >
             <div className="w-full max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <div className="flex flex-col items-center w-80 py-5">
                 <img
@@ -17,13 +41,19 @@ function Drivers() {
                   alt={driver.name}
                 />
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                 {driver.name}
+                  {driver.name}
                 </h5>
                 <p className="text-sm mt-2 text-gray-500 dark:text-gray-400">
-                  Experience <span className="font-bold text-yellow-400">{driver.experience}</span>
+                  Experience{" "}
+                  <span className="font-bold text-yellow-400">
+                    {driver.experience}
+                  </span>
                 </p>
                 <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                  Availability <span className="font-bold text-green-500">{driver.availability}</span>
+                  Availability{" "}
+                  <span className="font-bold text-green-500">
+                    {driver.availability}
+                  </span>
                 </p>
               </div>
             </div>
